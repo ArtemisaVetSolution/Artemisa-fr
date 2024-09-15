4162bbcfc06becb816bde229cfa50166a5b0f8eb
Alejandro Ramirez <alejorp195@hotmail-com>
2024-09-14 23:17:01 -0500

    kick off

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    // Todo: Add route with not found page - snackbar providers - mui theme provider - and guards
+      <Route>
+
+      </Route>
+  )
+}
+
+export default App
