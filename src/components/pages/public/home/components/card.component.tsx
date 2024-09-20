e012ba26b1444a5449685c8abfd7d48927bd2672
Alejandro Ramirez <alejorp195@hotmail-com>
2024-09-20 01:32:39 -0500

    add services to home

--- src/components/pages/public/home/components/card.component.tsx
+++ src/components/pages/public/home/components/card.component.tsx
@@ -1,17 +1,45 @@
-import { Paper } from "@mui/material"
+
+import { Paper, Typography } from "@mui/material"
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+
 
 interface IProps {
-  title: string
-  subtitle: string
-  description: string
+  name: string
+  // subtitle: string
+  // description: string
 }
 
-const Card = ({title, subtitle, description}: IProps) => {
+const Card = ({ name }: IProps) => {
+
+  const renderIcon = () => {
+    switch (name) {
+      case "Consulta general":
+        return <ControlPointIcon sx={{fontSize:"40px"}}/>;
+      case "Servicio 2":
+      case "Servicio 3":
+        
+      default:
+        return null;
+    }
+  };
   return (
-    <Paper sx={{width:'25%', height:'80%' }} elevation={5}>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>{description}</p>
+    <Paper sx={{
+      width: '120px',
+      height: '120px',
+      borderRadius: "50%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      transition: "all 0.3s ease",  // Suaviza la transición al hacer hover
+      '&:hover': {
+        boxShadow: 8,  // Aumenta la sombra
+        transform: "scale(1.1)",  // Agranda ligeramente el Paper
+      }
+
+    }} elevation={5}>
+      {renderIcon()      }
+      <Typography variant="h6" sx={{ textAlign: "center", fontSize:"16px" }}>{name}</Typography>
     </Paper>
   )
 }
