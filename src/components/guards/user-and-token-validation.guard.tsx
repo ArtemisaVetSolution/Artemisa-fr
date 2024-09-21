156267cad618f116618e166353621abf8d1e320c
Alejandro Ramirez P <alejorp195@hotmail.com>
2024-09-20 19:35:39 -0500

    guard in a 50 percent

--- src/components/guards/user-and-token-validation.guard.tsx
+++ src/components/guards/user-and-token-validation.guard.tsx
@@ -1,20 +1,53 @@
-import { useNavigate, Outlet } from "react-router-dom"
+import { IUser } from "@/models/interfaces";
+import { emptyUserState } from "@/state/redux/states";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate, Outlet, Navigate } from "react-router-dom"
+
+interface IProps {
+  privateValidation?: boolean;
+  isForAuth?: boolean,
+}
+interface IToken {
+  email?: string;
+  exp: number;
+  iat?: number;
+  id?: string;
+  role?: string;
+  permissions?: string[];
+}
 
 
-const Guard = () => {
+const Guard = ({isForAuth}: IProps) => {
 
   const navigate = useNavigate();
+  const token = sessionStorage.getItem('token');
+  let decodedToken: IToken = { exp: 0 };
+  let user: IUser = emptyUserState;
 
-  if(sessionStorage.getItem('token') === null || sessionStorage.getItem('user') === null){
+  if (token && token !== '') {
+    decodedToken = jwtDecode(token);
+    console.log(decodedToken);
+    if (decodedToken.exp * 1000 < new Date().getTime()) {
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
+      navigate('/login')
+    }
+  }
+  if( !token || sessionStorage.getItem('user') === null){
     navigate('/login')
   }
+  user = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user') ?? '') : emptyUserState;
+  
+  if(isForAuth){
+    if(user.id!== '')
+      return <Navigate to = {'/'}/>
+  }
+  
 
   
 
   return (
-    <div>
-      
-    </div>
+    <Outlet />
   )
 }
 
